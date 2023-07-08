d0778e0843e2259e04afec8bf9cb8d7d1e33c7a9
Ryunosuke_Ozaki <138109860+ryunosuke1990127@users.noreply.github.com>
2023-07-08 11:49:35 +0000

    Application_create_1

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+  def new
+    # Wiewへ渡すためのインスタンス変数を定義
+    @list = List.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  private
+  # ストロングパラメータの定義
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
